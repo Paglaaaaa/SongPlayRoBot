9cec1500f5575036a4d5ba8aa359bd66024002cd
Paglaaaaa <88768388+Paglaaaaa@users.noreply.github.com>
2021-08-11 17:18:42 +0530

    Create config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ API_ID = 7103848
 API_HASH = b9d7fd9a74488892695ffede4d597417
 BOT_TOKEN = 1766427674:AAHcSlfW-YezQqWwm2zdU3Cgk5LtHOpEJJ4
 DATABASE_URL = os.getenv("DATABASE_URL")
-OWNER_ID = list({int(x) for x in os.environ.get("OWNER_ID", "").split()})
+OWNER_ID = list({@mai_tera_heroint(x) for x in os.environ.get("OWNER_ID", "").split()})
